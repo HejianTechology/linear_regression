8cfd31889a671bd7d39794404da76a5a090b3f87
chizhanyuefeng <chizhanyuefeng@gmail.com>
2017-11-20 21:05:36 +0800

    增加数据集，并完善readme

--- data.py
+++ data.py
@@ -6,6 +6,6 @@ def get_dataset():
     获取线性数据集
     :return:返回x,y的数据
     """
-    x = np.random.rand(100).astype(np.float32)
+    x = np.random.rand(1000).astype(np.float32)
     y = 4.8 *x + 8.5
     return x,y
