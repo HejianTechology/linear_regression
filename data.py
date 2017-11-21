87779568429069614b2e2323eff7d54c4ed657f8
chizhanyuefeng <chizhanyuefeng@gmail.com>
2017-11-21 16:44:41 +0800

    添加测试数据集，并完成误差和准确率的计算

--- data.py
+++ data.py
@@ -3,9 +3,16 @@ import numpy as np
 
 def get_dataset():
     """
-    获取线性数据集
-    :return:返回x,y的数据
+    获取线性训练数据集和测试数据集
+    :return:x_train_data，y_train_data 训练数据集
+            x_test_data，y_test_data 测试数据集
     """
-    x = np.random.rand(1000).astype(np.float32)
-    y = 4.8 *x + 8.5
-    return x,y
+    # 设置训练数据集
+    x_train_data = np.random.rand(1000).astype(np.float32)
+    y_train_data = 4.8 *x_train_data + 8.5
+
+    #设置测试数据集
+    x_test_data = np.random.rand(50).astype(np.float32)
+    y_test_data = 4.8 *x_test_data + 8.5
+
+    return x_train_data,y_train_data,x_test_data,y_test_data
--- run.py
+++ run.py
@@ -3,17 +3,22 @@ import data
 
 
 # 获取数据
-x_data,y_data = data.get_dataset()
+x_data,y_data,x_test_data,y_test_data = data.get_dataset()
 data_size = len(x_data)
 
 # 开始建立模型
 wight = tf.Variable(tf.random_uniform([1]),tf.float32)
 bias = tf.Variable(tf.zeros([1]),tf.float32)
-y_predict = tf.multiply(wight,x_data) + bias
+y_ = tf.multiply(wight,x_data) + bias
 
 # 计算loss,并进行梯度下降
-loss = tf.reduce_mean(tf.square(y_data-y_predict))
+loss = tf.reduce_mean(tf.square(y_data-y_))
 train = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+
+# 计算误差和准确率
+y_predict = wight*x_test_data + bias
+error = tf.reduce_mean(tf.abs(y_test_data - y_predict))
+accuracy = 1-error
 
 with tf.Session() as sess:
     # 初始化所有变量
@@ -24,4 +29,7 @@ with tf.Session() as sess:
         sess.run(train)
         if train_step%50 ==0:
             print(sess.run(wight),sess.run(bias))
+
+    # 打印误差
+    print(sess.run(error),sess.run(accuracy))
 
